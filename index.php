298b283c78e7023c2f99dce0d2dcb6404620785d
muhammad <mhdsemps@gmail.com>
2020-02-12 00:37:10 +0300

    centralizing code pt1

--- index.php
+++ index.php
@@ -1,3 +1,49 @@
+<?php 
+require_once('./private/functions.php');
+$errors = [];
+$username = '';
+$password = '';
+
+if(is_post_request()) {
+
+  $username = $_POST['username'] ?? '';
+  $password = $_POST['password'] ?? '';
+
+  // Validations
+  if(is_blank($username)) {
+    $errors[] = "Username cannot be blank.";
+  }
+  if(is_blank($password)) {
+    $errors[] = "Password cannot be blank.";
+  }
+
+  // if there were no errors, try to login
+  if(empty($errors)) {
+    // Using one variable ensures that msg is the same
+    $login_failure_msg = "Log in was unsuccessful.";
+
+    $admin = find_admin_by_username($username);
+    if($admin) {
+
+      if(password_verify($password, $admin['hashed_password'])) {
+        // password matches
+        log_in_admin($admin);
+        redirect_to(url_for('/staff/index.php'));
+      } else {
+        // username found, but password does not match
+        $errors[] = $login_failure_msg;
+      }
+
+    } else {
+      // no username found
+      $errors[] = $login_failure_msg;
+    }
+
+  }
+
+}
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -38,7 +84,7 @@
         <div class="card-body login-card-body">
           <p class="login-box-msg">Sign in to start your session</p>
 
-          <form action="manager.html" method="post">
+          <form action="index.php" method="post">
             <div class="input-group mb-3">
               <input type="email" class="form-control" placeholder="Email" />
               <div class="input-group-append">
---
+++ private/functions.php
@@ -0,0 +1,32 @@
+<?php
+function is_post_request() {
+    return $_SERVER['REQUEST_METHOD'] == 'POST';
+  }
+
+  function find_admin_by_username($username)
+{
+    global $db;
+
+    $sql = "SELECT * FROM admins ";
+    $sql .= "WHERE username='" . db_escape($db, $username) . "' ";
+    $sql .= "LIMIT 1";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    $admin = mysqli_fetch_assoc($result); // find first
+    mysqli_free_result($result);
+    return $admin; // returns an assoc. array
+}
+ // Performs all actions necessary to log in an admin
+ function log_in_admin($admin) {
+    // Renerating the ID protects the admin from session fixation.
+      session_regenerate_id();
+      $_SESSION['admin_id'] = $admin['id'];
+      $_SESSION['last_login'] = time();
+      $_SESSION['username'] = $admin['username'];
+      return true;
+    }
+
+    function is_blank($value) {
+      return !isset($value) || trim($value) === '';
+    }
+?>

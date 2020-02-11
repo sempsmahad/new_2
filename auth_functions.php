0545c96a9a8d7b0776f537270d89738d8023abbc
muhammad <mhdsemps@gmail.com>
2020-02-11 16:02:56 +0300

    initial commit

---
+++ auth_functions.php
@@ -0,0 +1,37 @@
+<?php
+
+  // Performs all actions necessary to log in an admin
+  function log_in_admin($admin) {
+  // Renerating the ID protects the admin from session fixation.
+    session_regenerate_id();
+    $_SESSION['admin_id'] = $admin['id'];
+    $_SESSION['last_login'] = time();
+    $_SESSION['username'] = $admin['username'];
+    return true;
+  }
+
+  // Performs all actions necessary to log out an admin
+  function log_out_admin() {
+    unset($_SESSION['admin_id']);
+    unset($_SESSION['last_login']);
+    unset($_SESSION['username']);
+    // session_destroy(); // optional: destroys the whole session
+    return true;
+  }
+
+
+  function is_logged_in() {
+   
+    return isset($_SESSION['admin_id']);
+  }
+
+ 
+  function require_login() {
+    if(!is_logged_in()) {
+      redirect_to(url_for('/staff/login.php'));
+    } else {
+    
+    }
+  }
+
+?>
---
+++ db_credentials.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_SERVER", "localhost");
+define("DB_USER", "semps");
+define("DB_PASS", "1234");
+define("DB_NAME", "globe_bank");
+
+?>

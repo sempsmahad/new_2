25069e0a6678ae21a01eda8d1f0c140752e7d57a
muhammad <mhdsemps@gmail.com>
2020-02-13 17:49:52 +0300

    new

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php redirect_to(url_for('/index.php'));?>
+<?php
+require_once('./private/initialize.php');
+redirect_to(url_for('/index.php'));
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
